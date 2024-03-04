8f52e794d50dd481db85fd4a66ce87e6b2250370
Yadian Betancourt <2130665@cegepmontpetit.ca>
2024-03-04 13:37:16 -0500

    container with buttons

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -30,98 +30,143 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        child: Container(
+          width: 400,
+          height: 500,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: Colors.blueGrey.shade100,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text('Welcome', style: TextStyle(fontSize: 30)),
+              _controllerButton(context),
+              _screenButton(context),
+              _permissionsButton(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _controllerButton(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const MainGruPage()),
+          ).then((value) => _resetServices());
+        },
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.red,
+          textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          disabledForegroundColor: Colors.blue.withOpacity(0.38),
+          disabledBackgroundColor: Colors.blue.withOpacity(0.12),
+          minimumSize: const Size(80, 80),
+          shadowColor: Colors.grey,
+          elevation: 5,
+          side: BorderSide(
+              color: Colors.redAccent.shade400,
+              width: 2,
+              style: BorderStyle.solid),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          tapTargetSize: MaterialTapTargetSize.padded,
+        ),
+        child: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            _permissionsButton(context),
-            _gruButton(context),
-            _minionButton(context),
+            Padding(
+              padding: EdgeInsets.only(right: 8.0),
+              child: Icon(Icons.gamepad),
+            ),
+            Text("Controller"),
           ],
         ),
       ),
     );
   }
 
-  Widget _minionButton(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const MainMinionPage()),
-        ).then((value) => _resetServices());
-      },
-      style: ElevatedButton.styleFrom(
-        foregroundColor: Colors.black,
-        backgroundColor: Colors.yellow,
-        textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        disabledForegroundColor: Colors.blue.withOpacity(0.38),
-        disabledBackgroundColor: Colors.blue.withOpacity(0.12),
-        minimumSize: const Size(200, 100),
-        shadowColor: Colors.grey,
-        elevation: 5,
-        side: const BorderSide(
-            color: Colors.blueAccent, width: 2, style: BorderStyle.solid),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30),
+  Widget _screenButton(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 20.0, left: 20.0, bottom: 20.0),
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const MainMinionPage()),
+          ).then((value) => _resetServices());
+        },
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.yellow,
+          textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          disabledForegroundColor: Colors.blue.withOpacity(0.38),
+          disabledBackgroundColor: Colors.blue.withOpacity(0.12),
+          minimumSize: const Size(80, 80),
+          shadowColor: Colors.grey,
+          elevation: 5,
+          side: const BorderSide(
+              color: Colors.blueAccent, width: 2, style: BorderStyle.solid),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          tapTargetSize: MaterialTapTargetSize.padded,
         ),
-        tapTargetSize: MaterialTapTargetSize.padded,
+        child: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(right: 8.0),
+              child: Icon(Icons.screenshot_monitor_rounded),
+            ),
+            Text("Screen"),
+          ],
+        ),
       ),
-      child: const Text("Mode Minion"),
     );
   }
 
   Widget _permissionsButton(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        askPermissions();
-      },
-      style: ElevatedButton.styleFrom(
-        foregroundColor: Colors.black,
-        backgroundColor: Colors.blueGrey,
-        textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        disabledForegroundColor: Colors.blue.withOpacity(0.38),
-        disabledBackgroundColor: Colors.blue.withOpacity(0.12),
-        minimumSize: const Size(200, 100),
-        shadowColor: Colors.grey,
-        elevation: 5,
-        side: const BorderSide(
-            color: Colors.blueAccent, width: 2, style: BorderStyle.solid),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
+    return Padding(
+      padding: const EdgeInsets.only(right: 20.0, left: 20.0),
+      child: ElevatedButton(
+        onPressed: () {
+          askPermissions();
+        },
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.yellow,
+          textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          disabledForegroundColor: Colors.blue.withOpacity(0.38),
+          disabledBackgroundColor: Colors.blue.withOpacity(0.12),
+          minimumSize: const Size(80, 80),
+          shadowColor: Colors.grey,
+          elevation: 5,
+          side: const BorderSide(
+              color: Colors.blueAccent, width: 2, style: BorderStyle.solid),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          tapTargetSize: MaterialTapTargetSize.padded,
         ),
-        tapTargetSize: MaterialTapTargetSize.padded,
-      ),
-      child: const Text("Permissions"),
-    );
-  }
-
-  Widget _gruButton(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const MainGruPage()),
-        ).then((value) => _resetServices());
-      },
-      style: ElevatedButton.styleFrom(
-        foregroundColor: Colors.white,
-        backgroundColor: Colors.red,
-        textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        disabledForegroundColor: Colors.blue.withOpacity(0.38),
-        disabledBackgroundColor: Colors.blue.withOpacity(0.12),
-        minimumSize: const Size(200, 100),
-        shadowColor: Colors.grey,
-        elevation: 5,
-        side: BorderSide(
-            color: Colors.redAccent.shade400,
-            width: 2,
-            style: BorderStyle.solid),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30),
+        child: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(right: 8.0),
+              child: Icon(Icons.wifi),
+            ),
+            Text("Permissions"),
+          ],
         ),
-        tapTargetSize: MaterialTapTargetSize.padded,
       ),
-      child: const Text("Mode Gru"),
     );
   }
 
