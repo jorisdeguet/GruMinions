c6974096a429e4b7b8ff8d2a4074fa53516d902f
2115470 <2115470@cegepmontpetit.ca>
2024-03-07 13:20:45 -0500

    PROGRESS : Removed modes

--- lib/modes/list_of_modes.dart
+++ lib/modes/list_of_modes.dart
@@ -11,14 +11,14 @@ import 'package:gru_minions/modes/tapelelapin.dart';
 
 List<GruMinionMode> listOfModes(Function send) {
   return [
-    SyncMode(sendToOthers: send),
-    PianoMode(sendToOthers: send),
-    Miroir(sendToOthers: send),
-    HalMode(sendToOthers: send),
-    GridMode(sendToOthers: send),
-    TapeLeLapin(sendToOthers: send),
-    SimonMode(sendToOthers: send),
+    //SyncMode(sendToOthers: send),
+    //PianoMode(sendToOthers: send),
+    //Miroir(sendToOthers: send),
+    //HalMode(sendToOthers: send),
+    //GridMode(sendToOthers: send),
+    //TapeLeLapin(sendToOthers: send),
+    //SimonMode(sendToOthers: send),
     FlameMode(sendToOthers: send),
-    MatrixMode(sendToOthers: send),
+    //MatrixMode(sendToOthers: send),
   ];
 }
