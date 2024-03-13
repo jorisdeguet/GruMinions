92255b074a5bd2e2ada163553cd9c3159e3e6d25
1932943 <1932943@cegepmontpetit.ca>
2024-03-13 14:26:58 -0400

    Ajout classe Door

---
+++ lib/modes/boxsmasher/components/world/boxsmasher_door.dart
@@ -0,0 +1,62 @@
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:gru_minions/modes/boxsmasher/components/world/boxsmasher_box.dart';
+
+import '../../boxsmasher_game.dart';
+import 'boxsmasher_ground.dart';
+
+class Door extends SpriteComponent with CollisionCallbacks, HasGameRef<BoxSmasherGame>{
+
+  Door({required size, required position})
+      : super(size: size, position: position) {
+    debugMode = true;
+  }
+
+  bool onGround = false;
+  bool onBox = false;
+  bool onPressed = false;
+
+  @override
+  Future<void> onLoad() async {
+    RectangleHitbox shape = RectangleHitbox();
+    shape.collisionType = CollisionType.active;
+    add(shape);
+    await super.onLoad();
+  }
+
+  @override
+  void onCollision(intersectionPoints, other){
+    super.onCollision(intersectionPoints, other);
+    final points = intersectionPoints;
+    final otherBoxPoint = other.topLeftPosition;
+
+    if (other is Ground){
+      onGround = true;
+    }
+  }
+
+  @override
+  void onCollisionEnd(other){
+    super.onCollisionEnd(other);
+
+    if (other is Ground){
+      onGround = false;
+    }
+  }
+
+  @override
+  void update(double delta) {
+    super.update(delta);
+    buttonPressed(onPressed);
+  }
+
+  void buttonPressed(bool pressed){
+    if(onBox == true && onGround == false && pressed){
+      return;
+    }
+
+    if (onGround && pressed){
+      removeFromParent();
+    }
+  }
+}
